03bd5a07f18999e0c2317e7552fdfd1086b6f81a
Sonal Mittal <62133890+Sonal0409@users.noreply.github.com>
2026-09-13 10:02:39 +0530

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,11 @@
+
+if [ -f "/etc/passwd" ]; then
+echo "File exists"
+else
+echo "Files doesnot exist"
+fi
+if [ -d "/tmp/demo" ]; then
+echo "Directory exists"
+else
+echo "Directory doesnot exist"
+fi
